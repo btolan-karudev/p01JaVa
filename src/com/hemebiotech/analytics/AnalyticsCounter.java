f811fc16c2405765d9a36863a1df193e405d1281
Beniamin Tolan thinkCenter <beniamin.tolan@outlook.com>
2020-12-14 22:24:55 +0100

    dilated and constricted pupils bug

--- src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -5,37 +5,39 @@ import java.io.FileReader;
 import java.io.FileWriter;
 
 public class AnalyticsCounter {
-	private static int headacheCount = 0;	// initialize to 0
-	private static int rashCount = 0;		// initialize to 0
-	private static int pupilCount = 0;		// initialize to 0
-	
-	public static void main(String args[]) throws Exception {
-		// first get input
-		BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"));
-		String line = reader.readLine();
+    private static int headacheCount = 0;    // initialize to 0
+    private static int rashCount = 0;        // initialize to 0
+    private static int pupilCount = 0;        // initialize to 0
+    private static int pupilCCount = 0;        // initialize to 0
+
+    public static void main(String args[]) throws Exception {
+        // first get input
+        BufferedReader reader = new BufferedReader(new FileReader("symptoms.txt"));
+        String line = reader.readLine();
 
 
-		while (line != null) {
-			System.out.println("symptom from file: " + line);
-			if (line.equals("headache")) {
-				headacheCount++;
-				System.out.println("number of headaches: " + headacheCount);
-			}
-			else if (line.equals("rash")) {
-				rashCount++;
-			}
-			else if (line.contains("pupils")) {
-				pupilCount++;
-			}
+        while (line != null) {
+            System.out.println("symptom from file: " + line);
+            if (line.equals("headache")) {
+                headacheCount++;
+                System.out.println("number of headaches: " + headacheCount);
+            } else if (line.equals("rash")) {
+                rashCount++;
+            } else if (line.contains("dilated pupils")) {
+                pupilCount++;
+            } else if (line.contains("constricted pupils")) {
+                pupilCCount++;
+            }
 
-			line = reader.readLine();	// get another symptom
-		}
-		
-		// next generate output
-		FileWriter writer = new FileWriter ("result.out");
-		writer.write("headache: " + headacheCount + "\n");
-		writer.write("rash: " + rashCount + "\n");
-		writer.write("dialated pupils: " + pupilCount + "\n");
-		writer.close();
-	}
+            line = reader.readLine();    // get another symptom
+        }
+
+        // next generate output
+        FileWriter writer = new FileWriter("result.out");
+        writer.write("headache: " + headacheCount + "\n");
+        writer.write("rash: " + rashCount + "\n");
+        writer.write("dilated pupils: " + pupilCount + "\n");
+        writer.write("constricted pupils: " + pupilCCount + "\n");
+        writer.close();
+    }
 }
